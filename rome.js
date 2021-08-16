69d18a6dd6a6037382cb3d69c48c8da51f196d03
Melusi19 <mmmavuso79@gmail.com>
2021-08-16 19:38:55 +0200

    first commit

---
+++ rome.js
@@ -0,0 +1,41 @@
+function ArabicToRoman(num) {
+    let roman = "";
+    const roNum = {
+        M:1000,
+        CM:900, 
+        D:500,
+        CD:400, 
+        C:100, 
+        XC:90,
+        L:50, 
+        XV:40, 
+        X:10, 
+        IX:9, 
+        V:5, 
+        IV:4, 
+        I:1
+    };
+    let a;
+    
+    if(num < 1 || num > 3999)
+    return "Enter a number between 1 and 3999";
+    else {
+        for(let b in roNum) {
+            a = Math.floor(num / roNum[b]);
+            if (a >= 0) {
+                for(let i = 0; i < a; i++) {
+                    roman += b;
+                }
+            }
+            num = num % roNum[b];
+        }
+    }
+    return roman;
+}
+
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+const sum = num1 + num2;
+
+console.log(ArabicToRoman())
