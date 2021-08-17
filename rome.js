7b1ddb6bbab04fd69db88768473a463ee62dd505
Melusi19 <69846426+Melusi19@users.noreply.github.com>
2021-08-18 01:29:06 +0200

    Update rome.js

--- rome.js
+++ rome.js
@@ -33,9 +33,6 @@ function ArabicToRoman(num) {
     return roman;
 }
 
-const num1 = parseInt(prompt('Enter the first number '));
-const num2 = parseInt(prompt('Enter the second number '));
-
 const sum = num1 + num2;
 
 console.log(ArabicToRoman())
